fc291f908fa2c5ae546be2c1de078b4a105c5de9
Aaron Bedra <aaron@aaronbedra.com>
2015-11-01 12:25:25 -0600

    Fix test

--- paginate_test.go
+++ paginate_test.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"testing"
+        "testing"
 )
 
-func TestGeneratePaginationLinks(t *testing.T) {
-	actors := []string{"1.1.1.1","1.1.1.2",	"1.1.1.3","1.1.1.4","1.1.1.5"}
-	pages := GeneratePaginationLinks(actors, 1, 0, "blacklist")
-	if len(pages) != 5 {
-		t.Error("Expected 5 pages, got:", len(pages))
-	}
+func PopulateRedis() {
+        connection := connect("localhost", 6379)
+        connection.Cmd("FLUSHDB")
+        connection.Cmd("SET", "1.1.1.1:repsheet:ip:blacklisted", "test")
+        connection.Cmd("SET", "1.1.1.2:repsheet:ip:blacklisted", "test")
+        connection.Cmd("SET", "1.1.1.3:repsheet:ip:blacklisted", "test")
+        connection.Cmd("SET", "1.1.1.4:repsheet:ip:blacklisted", "test")
+        connection.Cmd("SET", "1.1.1.5:repsheet:ip:blacklisted", "test")
+        connection.Close()
+}
 
-	if pages["5"] != "/blacklist?page=5" {
-		t.Error("Expected /blacklist?page=5, got:", pages["5"])
-	}
+func TestGeneratePaginationLinks(t *testing.T) {
+        PopulateRedis()
+        connection := connect("localhost", 6379)
+        actors := connection.Cmd("KEYS", "*:repsheet:ip:blacklisted")
+        pages := GeneratePaginationLinks(actors, 1, 0, "blacklist")
+        if len(pages) != 5 {
+                t.Error("Expected 5 pages, got:", len(pages))
+        }
+
+        if pages["5"] != "blacklist?page=5" {
+                t.Error("Expected blacklist?page=5, got:", pages["5"])
+        }
 }
