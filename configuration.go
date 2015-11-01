534048473920b4a98925186a11592dd83af4c343
Aaron Bedra <aaron@aaronbedra.com>
2015-11-01 12:17:33 -0600

    gofmt

--- configuration.go
+++ configuration.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+        "fmt"
+        "net/http"
 )
 
 type Configuration struct {
-	Redis         Redis
-	LogFile       string
-	Port          int
-	GeoIPDatabase string
-	AssetsDir     string
+        Redis         Redis
+        LogFile       string
+        Port          int
+        GeoIPDatabase string
+        AssetsDir     string
 }
 
 type configurationHandler struct {
-	*Configuration
-	h func(*Configuration, http.ResponseWriter, *http.Request) (int, error)
+        *Configuration
+        h func(*Configuration, http.ResponseWriter, *http.Request) (int, error)
 }
 
 func (h configurationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.h(h.Configuration, w, r)
+        h.h(h.Configuration, w, r)
 }
 
 func (c Configuration) TemplateFor(name string) string {
-	return fmt.Sprintf("%s/templates/%s.html", c.AssetsDir, name)
+        return fmt.Sprintf("%s/templates/%s.html", c.AssetsDir, name)
 }
