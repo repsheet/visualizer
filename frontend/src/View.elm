4619fd9b3a54cc23b6441d68049df3b70252d199
Aaron Bedra <aaron@aaronbedra.com>
2017-12-15 09:09:20 -0600

    Render actor lists

--- frontend/src/View.elm
+++ frontend/src/View.elm
@@ -1,10 +1,10 @@
 module View exposing (..)
 
-import Html exposing (Html, div, h4, h1, a, text, span, i)
+import Html exposing (Html, div, h4, h3, h1, a, text, span, i, tr, th, thead, table, td, tbody)
 import Html.Attributes exposing (id, class, href)
 import RemoteData exposing (WebData)
 
-import Models exposing (Model, Dashboard)
+import Models exposing (Model, Dashboard, Actor)
 import Msgs exposing (Msg)
 
 maybeRender : WebData Dashboard -> Html Msg
@@ -38,6 +38,55 @@ statusBlock color section count =
           ]
         ]
 
+maybeRenderList : WebData Dashboard -> Html Msg
+maybeRenderList response =
+    case response of
+        RemoteData.NotAsked ->
+            text ""
+        RemoteData.Loading ->
+            text "Loading..."
+        RemoteData.Success dashboard ->
+            div [ class "row" ]
+                [ listBlock "Blacklisted" dashboard.blacklist
+                , listBlock "Marked"      dashboard.marklist
+                , listBlock "Whitelisted" dashboard.whitelist ]
+        RemoteData.Failure error ->
+            Debug.log (toString error)
+            text "ERROR"
+
+actorRow : Actor -> Html Msg
+actorRow actor =
+    tr []
+        [ td [] [ text actor.address ]
+        , td [] [ text actor.reason ]
+        ]
+
+listBlock : String -> List Actor -> Html Msg
+listBlock heading actors =
+    div [ class "col-md-4" ]
+        [ div [ class "portlet" ]
+              [ div [ class "portlet-header" ]
+                    [ h3 [] [ text (heading ++ " Actors") ] ]
+              , div [ class "portlet-content" ]
+                    [ div [ class "table-responsive" ]
+                          [table [ class "table" ]
+                               [ thead []
+                                     [ tr []
+                                       [ th [] [ text "IP Address"]
+                                       , th [] []
+                                       ]
+                                     ]
+                               , tbody [] (List.map actorRow actors)
+                               ]
+                          ]
+                    ]
+              ]
+        ]
+
+listBlockSection : WebData Dashboard -> Html Msg
+listBlockSection dashboard =
+    maybeRenderList dashboard
+
 view : Model -> Html Msg
 view model =
     div []
@@ -46,5 +95,6 @@ view model =
         , div [ id "content-container" ]
             [ div [] [ h4 [] [ text "Summary" ] ]
             , statusBlockSection model.dashboard
+            , listBlockSection model.dashboard
             ]
         ]
